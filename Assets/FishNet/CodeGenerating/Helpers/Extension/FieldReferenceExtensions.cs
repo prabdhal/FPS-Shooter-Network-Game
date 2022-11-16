b621e139119e41381fae8ee301d903914012ceef
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2022-11-16 15:43:19 -0500

    Imported FishNet

---
+++ Assets/FishNet/CodeGenerating/Helpers/Extension/FieldReferenceExtensions.cs
@@ -0,0 +1,31 @@
+﻿using MonoFN.Cecil;
+
+namespace FishNet.CodeGenerating.Helping.Extension
+{
+
+    internal static class FieldReferenceExtensions
+    {
+
+        /// <summary>
+        /// Gets a Resolve favoring cached results first.
+        /// </summary>
+        internal static FieldDefinition CachedResolve(this FieldReference fieldRef, CodegenSession session)
+        {
+            return session.GetClass<GeneralHelper>().GetFieldReferenceResolve(fieldRef);
+        }
+
+
+        public static FieldReference MakeHostGenericIfNeeded(this FieldReference fd, CodegenSession session)
+        {
+            if (fd.DeclaringType.HasGenericParameters)
+            {
+                return new FieldReference(fd.Name, fd.FieldType, fd.DeclaringType.CachedResolve(session).ConvertToGenericIfNeeded());
+            }
+
+            return fd;
+        }
+
+
+    }
+
+}
---
+++ Assets/FishNet/Runtime/Serializing/Helping/Attributes.cs
@@ -0,0 +1,21 @@
+﻿using FishNet.Utility.Constant;
+using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo(UtilityConstants.CODEGEN_ASSEMBLY_NAME)]
+namespace FishNet.Serializing.Helping
+{
+    /// <summary>
+    /// Method or type will be made public by codegen.
+    /// </summary>
+    internal class CodegenMakePublicAttribute : Attribute { }
+    /// <summary>
+    /// Type will be excluded from codegen.
+    /// </summary>
+    public class CodegenExcludeAttribute : Attribute { }
+    ///// <summary>
+    ///// Type will be included in codegen without needing to have any references.
+    ///// </summary>
+    //internal class CodegenIncludeAttribute : Attribute { }
+
+}
