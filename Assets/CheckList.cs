3633ae64674dd4cb111605bb599f920b30b51c35
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2022-11-18 17:46:05 -0500

    Updated CheckList.cs

--- Assets/CheckList.cs
+++ Assets/CheckList.cs
@@ -6,6 +6,8 @@
  * > when holding the Fire button, a raycast will be active and trigger damage depending on weapon "Fire rate"
  * Create crosshair for aim and fire
  * Add Reload functionality 
+ * Add Team colours and option to change teams via buttons
+ * Add UI to show player is reloading
  *
  *
  *
@@ -17,10 +19,9 @@
  * == Weapon
  * Add Pick up weapon functionality
  * Update automatic weapon click fire vs general fire rate (faster to click and fire...)
- * Add UI to show player is reloading
- * Add Team colours and option to change teams via buttons
- * 
  * Add aim down sight for some weapons like ARs, Sniper rifles
+ * Add friendly fire functionality so that same team members cannot damage eachother
+ * 
  * 
  * 
  * 
