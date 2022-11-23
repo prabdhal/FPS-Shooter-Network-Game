5b18a0e73ce2476bbbfe6f84f6dc331b23302164
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2022-11-23 13:44:15 -0500

    Updated CheckList.cs

--- Assets/CheckList.cs
+++ Assets/CheckList.cs
@@ -10,6 +10,7 @@
  * Add UI to show player is reloading
  * Add friendly fire functionality so that same team members cannot damage eachother
  * Show global chat where it shows who killed and died (has something to do with not recognizing ref from weapon script...)
+ * Add weapon accuracy with random points around raycast as bullets, the further the range... the worse the accuracy (larger circle)
  * 
  * BUG FIXES
  * When client joins... they don't see the color changes applied (WAS GETTING NULL OBJECT REF TO PLAYERHUD... BLOCKED OTHER CODE) 
@@ -35,21 +36,9 @@
  * Add Pick up weapon functionality
  * Update automatic weapon click fire vs general fire rate (faster to click and fire...)
  * Add aim down sight for some weapons like ARs, Sniper rifles
- * Add weapon accuracy with random points around raycast as bullets, the further the range... the worse the accuracy (larger circle)
- * Ex.
- *   Vector2 spreadDirection = Random.insideUnitCircle.normalized; //Get a random direction for the spread
-     Vector3 offsetDirection = new Vector3(fpsCam.transform.right.x * spreadDirection.x, fpsCam.transform.up * spreadDirection.y, 0); //Align direction with fps cam direction
- 
-     float offsetMagnitude = Random.Range(0f, maxSpreadAmount); //Get a random offset amount
-     offsetMagnitude = Mathf.Tan(offsetMagnitude); //Convert to segment length so we get desired degrees value
-     Vector3 bulletTrajectory = fpsCam.transform.forward + (offsetDirection * offsetMagnitude); //Add our offset to our forward vector
- 
-     RaycastHit hit;
-     if (Physics.Raycast(fpsCam.transform.position, bulletTrajectory, out hit, range))
-     {
-     }
  * 
  * Add functionality for burst shot
+ * Add Model and animations 
  * 
  * 
  * 
